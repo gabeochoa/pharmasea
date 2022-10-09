d0e8d4814f1731f069d37a54613447c827e2a43a
B1anky <basackstein@hotmail.com>
2022-10-09 16:26:45 -0400

    Fixed camera dragging in x direction.

--- camera.h
+++ camera.h
@@ -92,7 +92,7 @@ struct Cam {
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             auto mouseDelta = GetMouseDelta();
 
-            angle.x += mouseDelta.x / 100.0f;
+            angle.x -= mouseDelta.x / 100.0f;
             angle.y -= mouseDelta.y / 100.0f;
 
             angleMinMaxClamp();
