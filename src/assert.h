cdda8779e2a82ba1469d21f79508da400f279faf
gabeochoa <gabeochoa@meta.com>
2022-10-12 01:23:52 -0400

    make asserts crash program

--- src/assert.h
+++ src/assert.h
@@ -7,5 +7,6 @@
             std::cout << "Assertion failed: "; \
             std::cout << __VA_ARGS__;          \
             std::cout << std::endl;            \
+            assert(x);                         \
         }                                      \
     }
