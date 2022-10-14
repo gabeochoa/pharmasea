5429a88ac78cf6509b5923c55820276cfa36364b
gabeochoa <gabeochoa@meta.com>
2022-10-14 00:20:18 -0400

    dont need .get()

--- src/aboutlayer.h
+++ src/aboutlayer.h
@@ -60,7 +60,7 @@ struct AboutLayer : public Layer {
         root.growflags = ui::GrowFlags::Row;
 
         Widget left_padding(
-            {.mode = Pixels, .value = 100.f, .strictness = 0.9},
+            {.mode = Pixels, .value = 100.f, .strictness = 1.f},
             {.mode = Pixels, .value = WIN_H, .strictness = 1.f});
 
         Widget content({.mode = Children},
@@ -90,51 +90,26 @@ A game by:
     Alice
         )";
 
-        ui_context.get()->push_parent(&root);
+        ui_context->push_parent(&root);
         {
             padding(left_padding);
             div(content);
 
-            ui_context.get()->push_parent(&content);
+            ui_context->push_parent(&content);
             {
                 text(about_text, about_info);
                 button_with_label(back_button, "Back");
                 padding(button_padding);
                 button_with_label(back_button2, "Back");
             }
-            ui_context.get()->pop_parent();
+            ui_context->pop_parent();
         }
-        ui_context.get()->pop_parent();
+        ui_context->pop_parent();
 
         // after all children are done, process
-        std::cout << "process widget" << std::endl;
         autolayout::process_widget(&root);
-
-
-        std::cout << "render widget" << std::endl;
-        ui_context.get()->render_all();
-
-        // root.print_tree();
-
-        std::cout << "********************** END FRAME **************** " << std::endl;
-
-        /*
-        text_old(MK_UUID(id, ROOT_ID), WidgetConfig({
-                                       .position = vec2{50.f, 50.f},
-                                       .size = vec2{30.f, 5.f},
-                                       .text = about_info,
-                                   }));
-
-        if (button(MK_UUID(id, ROOT_ID), WidgetConfig({
-                                             .position = vec2{50.f, 400.f},
-                                             .size = vec2{100.f, 50.f},
-                                             .text = std::string("Back"),
-                                         }))) {
-            Menu::get().state = Menu::State::Root;
-        }
-
-        */
-
+        // now we have the info needed to render
+        ui_context->render_all();
         ui_context->end();
     }
 
