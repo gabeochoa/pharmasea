5a7702152cdd6eee5e3de89584e54d8d306db340
baja-blaster <alicemalatos@gmail.com>
2022-10-18 19:33:39 -0700

    added working headers for ailments/medicines/names

---
+++ src/ailment.h
@@ -0,0 +1,16 @@
+#pragma once
+//modifies person behavior, applies debuffs/buffs
+//hide from players, but for dev porpoises(haha) having a blip near the person could be useful for levelbuilding
+//can have multiple ailments / can use this structure to have slowed/fast characters for example.
+//Ex:
+//ailment
+    //speed modifier
+    //stumbling/shaking?
+    //visual effect modifier(?)
+    //infectivity(?)
+    //gross - other persons avoid a radius around them
+
+
+//Names
+//Hooked! - addiction debuff
+//
---
+++ src/medicine.h
@@ -0,0 +1,16 @@
+#pragma once
+//Medicines
+//Tunavil - Antiaddidctive (Hard Stuff)
+//Nicofin - Antiaddictive (Nicotine)
+//Pagliaccin - Antidepressant
+//Clamudone - Anti-anxiety
+//Salvemon - Skin repair (salve)
+//Troutox - Antitoxin
+//Leeches - Yeah
+//Dartfish Smokes - Cigarettes
+
+
+//Negative Effects
+//Addiction
+//Emergence of other symptoms
+//
