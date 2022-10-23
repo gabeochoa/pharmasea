68d1efe9d6ddf98f365188bbfc493cba9035a832
Gabe Ochoa <gabeochoa@meta.com>
2022-10-23 15:56:51 -0400

    remove external include from menuy

--- src/menu.h
+++ src/menu.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#include "external_include.h"
+#include <iostream>
 #include "singleton.h"
 
 SINGLETON_FWD(Menu)
--- src/random.h
+++ src/random.h
@@ -1,16 +1,15 @@
 
-#pragma once 
+#pragma once
 
 #include <random>
 
 inline int randIn(int a, int b) { return a + (std::rand() % (b - a + 1)); }
 
-inline int randSign() { 
-	int sign = randIn(0, 1);
-	if (sign == 0) {
-		return -1;
-	}
-	else {
-		return 1;
-	}
+inline int randSign() {
+    int sign = randIn(0, 1);
+    if (sign == 0) {
+        return -1;
+    } else {
+        return 1;
+    }
 }
