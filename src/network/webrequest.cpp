e10f09afeba70f3492ce6f362cc6281a5aebff14
Gabe Ochoa <gabeochoa@meta.com>
2022-10-31 00:34:31 -0400

    move webrequest to cpp (because it includes winsock.h)

---
+++ src/network/webrequest.cpp
@@ -0,0 +1,16 @@
+
+#include "webrequest.h"
+
+#include <HTTPRequest.hpp>
+
+namespace network {
+std::optional<std::string> get_remote_ip_address() {
+    try {
+        http::Request request{"http://api.ipify.org/"};
+        const auto response = request.send("GET");
+        return std::string{response.body.begin(), response.body.end()};
+    } catch (const std::exception& e) {
+        return {};
+    }
+}
+}  // namespace network
--- src/network/webrequest.h
+++ src/network/webrequest.h
@@ -1,20 +1,10 @@
 
 #pragma once
-
-#include <HTTPRequest.hpp>
 #include <iostream>
 #include <optional>
 
 namespace network {
 
-static std::optional<std::string> get_remote_ip_address() {
-    try {
-        http::Request request{"http://api.ipify.org/"};
-        const auto response = request.send("GET");
-        return std::string{response.body.begin(), response.body.end()};
-    } catch (const std::exception& e) {
-        return {};
-    }
-}
+std::optional<std::string> get_remote_ip_address();
 
 }  // namespace network
