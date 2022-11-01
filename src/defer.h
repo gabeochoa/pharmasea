a74371ea6006abbb2f650a4332697411fefb55ad
Gabe Ochoa <gabeochoa@meta.com>
2022-10-31 21:48:07 -0400

    add doc for defer usage

--- src/defer.h
+++ src/defer.h
@@ -4,5 +4,13 @@
 #include <functional>
 #include <memory>
 
+/*
+    for example:
+     {
+         auto X = new int[10];
+         defer(free x)
+         ... rest of function
+     } // free happens here
+*/
 #define defer(code) \
     std::shared_ptr<void> _(nullptr, std::bind([&]() { code; }));
