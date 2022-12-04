fe9597edaa8588358f3929320a2ea4f0847e57b2
Gabe Ochoa <gabeochoa@meta.com>
2022-12-03 22:03:28 -0500

    ai added some post processing effects :)

--- resources/shaders/post_processing.fs
+++ resources/shaders/post_processing.fs
@@ -17,7 +17,7 @@ const vec2 size = vec2(1280, 720);   // Framebuffer size
 const float samples = 10.0;          // Pixels per axis; higher = bigger glow, worse performance
 const float quality = 0.5;          // Defines size factor: Lower = smaller glow, better quality
 
-void main()
+vec4 bloom()
 {
     vec4 sum = vec4(0);
     vec2 sizeFactor = vec2(1)/size*quality;
@@ -36,5 +36,42 @@ void main()
     }
 
     // Calculate final fragment color
-    finalColor = ((sum/(samples*samples)) + source)*colDiffuse;
+    return ((sum/(samples*samples)) + source)*colDiffuse;
+}
+
+const float SATURATION_FACTOR = 1.1;
+
+// See screenshot added in this git blame, an AI wrote this!
+vec3 rgb2hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main()
+{
+    vec4 post_bloom = bloom();
+
+    // Convert to HSV color space
+    vec3 hsv = rgb2hsv(post_bloom.rgb);
+
+    // Increase saturation
+    hsv.y *= SATURATION_FACTOR;
+
+    // Convert back to RGB
+    vec4 saturated = vec4(hsv2rgb(hsv), post_bloom.a);
+
+    finalColor = saturated;
 }
