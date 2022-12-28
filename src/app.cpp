5ca1c0d74547c73093b25020233d0185bdab296b
Gabe Ochoa <gabeochoa@meta.com>
2022-12-28 16:45:30 -0500

    move app impl into cpp

--- src/app.cpp
+++ src/app.cpp
@@ -1,6 +1,15 @@
 
 #include "app.h"
 
+//
+#include "external_include.h"
+
+//
+
+#include "globals_register.h"
+#include "keymap.h"
+#include "profile.h"
+#include "raylib.h"
 #include "settings.h"
 #include "shader_library.h"
 
@@ -13,4 +22,121 @@ void App::start_post_processing() {
 void App::end_post_processing() {
     if (!Settings::get().data.enable_postprocessing) return;
     EndShaderMode();
+}
+
+App::App(const AppSettings& settings) {
+    // This goes above Init since that loves to spew errors
+    SetTraceLogLevel(settings.logLevel);
+    SetTargetFPS(settings.fps);
+    //
+    width = settings.width;
+    height = settings.height;
+
+    InitWindow(width, height, settings.title);
+
+    if (settings.onCreate) settings.onCreate();
+
+    KeyMap::get();
+
+    mainRT = LoadRenderTexture(width, height);
+    GLOBALS.set("mainRT", &mainRT);
+}
+
+App::~App() {
+    UnloadRenderTexture(mainRT);
+    // TODO do we need to / can we remove mainRT from globals
+}
+
+void App::pushLayer(Layer* layer) { layerstack.push(layer); }
+void App::pushOverlay(Layer* layer) { layerstack.pushOverlay(layer); }
+
+void App::onEvent(Event& event) {
+    EventDispatcher dispatcher(event);
+    dispatcher.dispatch<WindowResizeEvent>(
+        std::bind(&App::onWindowResize, this, std::placeholders::_1));
+}
+
+bool App::onWindowResize(WindowResizeEvent event) {
+    // std::cout << "Got Window Resize Event: " << event.width << ", "
+    // << event.height << std::endl;
+
+    width = event.width;
+    height = event.height;
+
+    SetWindowSize(width, height);
+
+    UnloadRenderTexture(mainRT);
+    mainRT = LoadRenderTexture(width, height);
+    GLOBALS.set("mainRT", &mainRT);
+    return true;
+}
+
+void App::processEvent(Event& e) {
+    this->onEvent(e);
+    if (e.handled) {
+        return;
+    }
+
+    // Have the top most layers get the event first,
+    // if they handle it then no need for the lower ones to get the rest
+    // eg imagine UI pause menu blocking game UI elements
+    //    we wouldnt want the player to click pass the pause menu
+    for (auto it = layerstack.end(); it != layerstack.begin();) {
+        (*--it)->onEvent(e);
+        if (e.handled) {
+            break;
+        }
+    }
+}
+
+void App::close() { running = false; }
+
+void App::run() {
+    running = true;
+    while (running && !WindowShouldClose()) {
+        float dt = GetFrameTime();
+        this->loop(dt);
+    }
+    CloseWindow();
+}
+
+void App::loop(float dt) {
+    PROFILE();
+
+    for (Layer* layer : layerstack) {
+        layer->onUpdate(dt);
+    }
+
+    draw_all_to_texture(dt);
+    render_to_screen();
+
+    // Check Input
+    {
+        KeyMap::get().forEachInputInMap(
+            std::bind(&App::processEvent, this, std::placeholders::_1));
+
+        KeyMap::get().forEachCharTyped(
+            std::bind(&App::processEvent, this, std::placeholders::_1));
+    }
+}
+
+void App::draw_all_to_texture(float dt) {
+    BeginTextureMode(mainRT);
+    for (Layer* layer : layerstack) {
+        layer->onDraw(dt);
+    }
+    EndTextureMode();
+}
+
+void App::render_to_screen() {
+    BeginDrawing();
+    {
+        start_post_processing();
+        {
+            DrawTextureRec(mainRT.texture, {0, 0, width * 1.f, -height * 1.f},
+                           {0, 0}, WHITE);
+        }
+        end_post_processing();
+    }
+    EndDrawing();
 }
--- src/app.h
+++ src/app.h
@@ -1,14 +1,8 @@
 
 #pragma once
 
-#include "external_include.h"
-//
 #include "event.h"
-#include "globals_register.h"
-#include "keymap.h"
 #include "layer.h"
-#include "profile.h"
-#include "raylib.h"
 #include "singleton.h"
 
 struct AppSettings {
@@ -32,124 +26,21 @@ struct App {
     // TODO create a render texture library?
     RenderTexture2D mainRT;
 
-    explicit App(const AppSettings& settings) {
-        width = settings.width;
-        height = settings.height;
+    explicit App(const AppSettings&);
+    ~App();
 
-        InitWindow(width, height, settings.title);
-
-        SetTargetFPS(settings.fps);
-        SetTraceLogLevel(settings.logLevel);
-
-        if (settings.onCreate) settings.onCreate();
-
-        KeyMap::get();
-
-        mainRT = LoadRenderTexture(width, height);
-        GLOBALS.set("mainRT", &mainRT);
-    }
-
-    ~App() {
-        UnloadRenderTexture(mainRT);
-        // TODO do we need to / can we remove mainRT from globals
-    }
-
-    void pushLayer(Layer* layer) { layerstack.push(layer); }
-    void pushOverlay(Layer* layer) { layerstack.pushOverlay(layer); }
-
-    void onEvent(Event& event) {
-        EventDispatcher dispatcher(event);
-        dispatcher.dispatch<WindowResizeEvent>(
-            std::bind(&App::onWindowResize, this, std::placeholders::_1));
-    }
-
-    bool onWindowResize(WindowResizeEvent event) {
-        // std::cout << "Got Window Resize Event: " << event.width << ", "
-        // << event.height << std::endl;
-
-        width = event.width;
-        height = event.height;
-
-        SetWindowSize(width, height);
-
-        UnloadRenderTexture(mainRT);
-        mainRT = LoadRenderTexture(width, height);
-        GLOBALS.set("mainRT", &mainRT);
-        return true;
-    }
-
-    void processEvent(Event& e) {
-        this->onEvent(e);
-        if (e.handled) {
-            return;
-        }
-
-        // Have the top most layers get the event first,
-        // if they handle it then no need for the lower ones to get the rest
-        // eg imagine UI pause menu blocking game UI elements
-        //    we wouldnt want the player to click pass the pause menu
-        for (auto it = layerstack.end(); it != layerstack.begin();) {
-            (*--it)->onEvent(e);
-            if (e.handled) {
-                break;
-            }
-        }
-    }
-
-    void close() { running = false; }
-
-    void run() {
-        running = true;
-        while (running && !WindowShouldClose()) {
-            float dt = GetFrameTime();
-            this->loop(dt);
-        }
-        CloseWindow();
-    }
-
-    void loop(float dt) {
-        PROFILE();
-
-        for (Layer* layer : layerstack) {
-            layer->onUpdate(dt);
-        }
-
-        draw_all_to_texture(dt);
-        render_to_screen();
-
-        // Check Input
-        {
-            KeyMap::get().forEachInputInMap(
-                std::bind(&App::processEvent, this, std::placeholders::_1));
-
-            KeyMap::get().forEachCharTyped(
-                std::bind(&App::processEvent, this, std::placeholders::_1));
-        }
-    }
+    void pushLayer(Layer* layer);
+    void pushOverlay(Layer* layer);
+    void onEvent(Event& event);
+    bool onWindowResize(WindowResizeEvent event);
+    void processEvent(Event& e);
+    void close();
+    void run();
+    void loop(float dt);
 
    private:
-    void draw_all_to_texture(float dt) {
-        BeginTextureMode(mainRT);
-        for (Layer* layer : layerstack) {
-            layer->onDraw(dt);
-        }
-        EndTextureMode();
-    }
-
+    void draw_all_to_texture(float dt);
     void start_post_processing();
     void end_post_processing();
-
-    void render_to_screen() {
-        BeginDrawing();
-        {
-            start_post_processing();
-            {
-                DrawTextureRec(mainRT.texture,
-                               {0, 0, width * 1.f, -height * 1.f}, {0, 0},
-                               WHITE);
-            }
-            end_post_processing();
-        }
-        EndDrawing();
-    }
+    void render_to_screen();
 };
