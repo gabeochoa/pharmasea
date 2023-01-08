28f23e9e39fc46b1557713d3cff39668e51ce072
Gabe Ochoa <gabeochoa@meta.com>
2023-01-08 17:22:16 -0500

    add bitsery to axiswithdir

--- src/engine/gamepad_axis_with_dir.h
+++ src/engine/gamepad_axis_with_dir.h
@@ -1,9 +1,18 @@
 
 #pragma once
 
+#include "../vendor_include.h"
 #include "raylib.h"
 
 struct GamepadAxisWithDir {
     GamepadAxis axis;
     float dir = -1;
+
+   private:
+    friend bitsery::Access;
+    template<typename S>
+    void serialize(S& s) {
+        s.value4b(axis);
+        s.value4b(dir);
+    }
 };
