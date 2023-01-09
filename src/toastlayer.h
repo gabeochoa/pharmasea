f526d62f9577074b8ece6ee70a156b8ee70c52f1
Gabe Ochoa <gabeochoa@meta.com>
2023-01-09 00:01:27 -0500

    use fontsizer to place text inside toast

--- src/toastlayer.h
+++ src/toastlayer.h
@@ -3,16 +3,17 @@
 
 #include "external_include.h"
 //
+#include "engine/font_sizer.h"
 #include "engine/layer.h"
 #include "engine/ui_theme.h"
 //
 #include "toastmanager.h"
 
-struct ToastLayer : public Layer {
-    ToastLayer() : Layer("Toasts") {}
+struct ToastLayer : public Layer, public FontSizeCache {
+    ToastLayer() : Layer("Toasts") { FontSizeCache::init(); }
 
-    const float toastPctHeight = 0.10f;
-    const float toastPctWidth = 0.10f;
+    const float toastPctHeight = 0.05f;
+    const float toastPctWidth = 0.25f;
     const float toastPctBottomPadding = 0.01f;
 
     virtual void onEvent(Event&) override {}
@@ -27,29 +28,42 @@ struct ToastLayer : public Layer {
         const float toastWidth = (WIN_WF() * toastPctWidth);
         const float toastHeight = (WIN_HF() * toastPctHeight);
         float offY = 0.f;
+        float spacing = 0.f;
+
         for (auto& toast : TOASTS) {
             Color primary = ui::DEFAULT_THEME.from_usage(ui::theme::Primary);
             Color accent = ui::DEFAULT_THEME.from_usage(ui::theme::Accent);
+            Color font_color = ui::DEFAULT_THEME.from_usage(ui::theme::Font);
             // TODO interpolate the alpha on these to look nicer, (pop in / pop
             // out)
 
+            vec2 pos = {
+                WIN_WF() * 0.72f,          //
+                (WIN_HF() * 0.1f) + offY,  //
+            };
+
             DrawRectangleRounded(
                 Rectangle{
-                    WIN_WF() * 0.8f,           //
-                    (WIN_HF() * 0.1f) + offY,  //
-                    toastWidth,                //
-                    toastHeight,               //
+                    pos.x,        //
+                    pos.y,        //
+                    toastWidth,   //
+                    toastHeight,  //
                 },
                 0.15f, 4, primary);
 
             DrawRectangleRounded(
                 Rectangle{
-                    WIN_WF() * 0.8f,                                    //
-                    ((WIN_HF() * 0.1f) + offY) + (toastHeight * 0.9f),  //
-                    toastWidth * toast.pctOpen,                         //
-                    toastHeight * 0.1f,                                 //
+                    pos.x,                         //
+                    pos.y + (toastHeight * 0.9f),  //
+                    toastWidth * toast.pctOpen,    //
+                    toastHeight * 0.1f,            //
                 },
                 0.15f, 4, accent);
+
+            float font_size = get_font_size(std::string(toast.msg), toastWidth,
+                                            toastHeight, spacing);
+            DrawTextEx(font, std::string(toast.msg).c_str(), pos, font_size,
+                       spacing, font_color);
 
             offY += (bot_padd + toastHeight);
         }
