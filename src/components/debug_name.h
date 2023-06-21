955a044303acff878002b2d175db2fbb5a5201d4
Gabe Ochoa <gabeochoa@meta.com>
2023-06-20 20:32:12 -0400

    debug name

---
+++ src/components/debug_name.h
@@ -0,0 +1,28 @@
+
+#pragma once
+
+//
+#include "base_component.h"
+
+struct DebugName : public BaseComponent {
+    virtual ~DebugName() {}
+
+    void update(const std::string& new_name) {
+        _name = new_name;
+        name_length = (int) _name.size();
+    }
+
+    const std::string name() const { return _name; }
+
+   private:
+    int name_length = 1;
+    std::string _name = "";
+
+    friend bitsery::Access;
+    template<typename S>
+    void serialize(S& s) {
+        s.ext(*this, bitsery::ext::BaseClass<BaseComponent>{});
+        s.value4b(name_length);
+        s.text1b(_name, name_length);
+    }
+};
