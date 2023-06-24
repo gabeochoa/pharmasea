fb080e5adad7abab66891a125f33033b68d17a55
Gabe Ochoa <gabeochoa@meta.com>
2023-06-24 14:08:07 -0400

    add background ui

--- src/layers/playerinfolayer.h
+++ src/layers/playerinfolayer.h
@@ -16,18 +16,25 @@ struct PlayerInfoLayer : public Layer {
     virtual void onUpdate(float) override {}
 
     void draw_text(const std::string& str) {
-        float y = 100 + y_pos;
+        float y = 200 + y_pos;
         DrawTextEx(Preload::get().font, str.c_str(), vec2{5, y}, 20, 0, WHITE);
         y_pos += 15;
     }
 
     virtual void onDraw(float) override {
-        Entity* me = GLOBALS.get_ptr<Entity>("player");
+        if (!MenuState::s_in_game()) return;
+
+        Entity* me = GLOBALS.get_ptr<Entity>("active_camera_target");
         if (!me) return;
+
+        DrawRectangle(5, 200, 175, 75, (Color){50, 50, 50, 200});
 
         y_pos = 0;
         draw_text("PlayerInfo:");
         draw_text(fmt::format("position: {}", me->get<Transform>().pos()));
+        draw_text(
+            fmt::format("holding furniture?: {}",
+                        me->get<CanHoldFurniture>().is_holding_furniture()));
         draw_text(fmt::format("holding item?: {}",
                               me->get<CanHoldItem>().is_holding_item()));
     }
