15e3e02d4d5841bb438ec5a4c8ce4f23620b218b
Gabe Ochoa <gabeochoa@meta.com>
2023-06-28 22:54:22 -0400

    add another customer

--- src/level_info.h
+++ src/level_info.h
@@ -301,6 +301,13 @@ struct GameMapInfo : public LevelInfo {
             }
 
             {
+                const auto location = vec2{-12 * TILESIZE, -12 * TILESIZE};
+                std::shared_ptr<Entity> customer;
+                customer.reset(make_customer(vec::to3(location)));
+                EntityHelper::addEntity(dynamic_pointer_cast<Entity>(customer));
+            }
+
+            {
                 const auto location = vec2{-15 * TILESIZE, -15 * TILESIZE};
                 std::shared_ptr<Entity> customer;
                 customer.reset(make_customer(vec::to3(location)));
