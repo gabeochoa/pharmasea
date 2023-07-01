8e29c7f81d37254ecc013de746258a2f8406c26e
Gabe Ochoa <gabeochoa@meta.com>
2023-07-01 00:21:22 -0400

    add info about who is in there

--- src/components/is_trigger_area.h
+++ src/components/is_trigger_area.h
@@ -6,8 +6,20 @@ struct IsTriggerArea : public BaseComponent {
     virtual ~IsTriggerArea() {}
 
     [[nodiscard]] const std::string& title() const { return _title; }
+    [[nodiscard]] bool should_wave() const {
+        log_info("should wave? cur{} wanted{}", current_entrants,
+                 wanted_entrants);
+        return has_min_matching_entrants();
+    }
+    [[nodiscard]] bool has_matching_entrants() const {
+        return current_entrants == wanted_entrants;
+    }
 
-    void update_title(const std::string& nt) {
+    [[nodiscard]] bool has_min_matching_entrants() const {
+        return current_entrants >= wanted_entrants;
+    }
+
+    auto& update_title(const std::string& nt) {
         _title = nt;
         if ((int) _title.size() > max_title_length) {
             log_warn(
@@ -16,9 +28,32 @@ struct IsTriggerArea : public BaseComponent {
                 max_title_length, _title.size());
             _title.resize(max_title_length);
         }
+        return *this;
+    }
+
+    auto& update_entrants(int ne) {
+        current_entrants = ne;
+        return *this;
+    }
+
+    auto& update_max_entrants(int ne) {
+        wanted_entrants = ne;
+        return *this;
+    }
+
+    auto& increment_entrants() {
+        if (current_entrants < wanted_entrants) current_entrants += 100;
+        return *this;
+    }
+
+    auto& decrement_entrants() {
+        if (current_entrants > 0) current_entrants -= 1;
+        return *this;
     }
 
    private:
+    int wanted_entrants;
+    int current_entrants;
     std::string _title;
     int max_title_length = 20;
 
@@ -26,6 +61,9 @@ struct IsTriggerArea : public BaseComponent {
     template<typename S>
     void serialize(S& s) {
         s.ext(*this, bitsery::ext::BaseClass<BaseComponent>{});
+
+        s.value4b(wanted_entrants);
+        s.value4b(current_entrants);
 
         s.value4b(max_title_length);
         s.text1b(_title, max_title_length);
