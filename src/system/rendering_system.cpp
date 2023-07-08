ec9d60da7488f52c0d6f5a3e740513ac3ac9c27c
Gabe Ochoa <gabeochoa@meta.com>
2023-07-07 20:01:23 -0400

    place in the top right

--- src/system/rendering_system.cpp
+++ src/system/rendering_system.cpp
@@ -7,13 +7,14 @@ namespace system_manager {
 namespace ui {
 
 void render_networked_players(const Entities& entities, float dt) {
-    float x_pos = WIN_WF() - 300;
-    float y_pos = 200.f;
+    float x_pos = WIN_WF() - 150;
+    float y_pos = 20.f;
 
     auto _draw_text = [&](const std::string& str) mutable {
-        DrawTextEx(Preload::get().font, str.c_str(), vec2{x_pos, y_pos}, 40, 0,
-                   WHITE);
-        y_pos += 50;
+        int size = 20;
+        DrawTextEx(Preload::get().font, str.c_str(), vec2{x_pos, y_pos}, size,
+                   0, WHITE);
+        y_pos += (size * 1.25f);
     };
 
     const auto _render_single_networked_player =
@@ -31,9 +32,10 @@ void render_networked_players(const Entities& entities, float dt) {
             entity->get<ModelRenderer>().model_info().value().model_name;
         raylib::Texture texture =
             TextureLibrary::get().get(fmt::format("{}_mug", model_name));
-        float scale = 0.09f;
-        raylib::DrawTextureEx(texture, {x_pos - (500 * scale), y_pos}, 0, scale,
-                              WHITE);
+        float scale = 0.06f;
+        raylib::DrawTextureEx(texture,
+                              {x_pos - (500 /* image size */ * scale), y_pos},
+                              0, scale, WHITE);
     };
 
     for (auto& entity : entities) {
