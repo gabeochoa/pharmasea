5c59a7010e6bda84b5cc10667bc6ca79306bccac
B1anky <basackstein@hotmail.com>
2023-07-09 01:56:37 -0400

    be - made the json parsing for the config settings safe if json is improperly formatted.

--- src/preload.cpp
+++ src/preload.cpp
@@ -13,9 +13,13 @@ void Preload::load_config() {
         Files::get().fetch_resource_path(std::get<0>(configFilePath),
                                          std::get<1>(configFilePath)));
     if (ifs.good()) {
-        const auto configJSON = nlohmann::json::parse(ifs);
-        LOG_LEVEL = configJSON.value("LOG_LEVEL", 2);
-        std::cout << "LOG_LEVEL read from file: " << LOG_LEVEL << std::endl;
+        try {
+            const auto configJSON = nlohmann::json::parse(ifs);
+            LOG_LEVEL = configJSON.value("LOG_LEVEL", 2);
+            std::cout << "LOG_LEVEL read from file: " << LOG_LEVEL << std::endl;
+        } catch (const std::exception& e) {
+            std::cerr << "Preload::load_config: settings.json formatted improperly. " << e.what() << std::endl;
+        }
     }else{
         std::cerr << "load_config error: configFilePath not found, "
                         "streamer couldn't open file. Check path: "
