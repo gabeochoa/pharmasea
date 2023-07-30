3dc9d94b559794c727732eced25acd8dc36eefcb
Gabe Ochoa <gabeochoa@meta.com>
2023-07-29 22:53:26 -0400

    change to using system clock

--- src/engine/time.h
+++ src/engine/time.h
@@ -8,7 +8,7 @@ namespace now {
 
 inline long long current_ms() {
     return std::chrono::time_point_cast<std::chrono::milliseconds>(
-               std::chrono::high_resolution_clock::now())
+               std::chrono::system_clock::now())
         .time_since_epoch()
         .count();
 }
