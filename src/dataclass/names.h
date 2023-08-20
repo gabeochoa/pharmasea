ee1ea22c23afbaa325834315ded34e6c79713ed2
Gabe Ochoa <gabeochoa@meta.com>
2023-08-20 03:53:19 -0400

    add a bunch of names

--- src/dataclass/names.h
+++ src/dataclass/names.h
@@ -4,6 +4,27 @@
 #include "../engine/random.h"
 #include "../external_include.h"
 
+const std::array<const char*, 40> alcohol_names = {
+    "Martini", "Ale",       "Chardonnay", "Brandy",   "Tequila",  "Lager",
+    "Rosé",    "Mojito",    "Whiskey",    "Gin",      "Mimosa",   "Rum",
+    "Bourbon", "Shiraz",    "Margarita",  "Merlot",   "Cosmo",    "Aperol",
+    "Shandy",  "Bellini",   "Cider",      "Julep",    "Cabernet", "Sambuca",
+    "Rye",     "Pinot",     "Fizz",       "Daiquiri", "Pina",     "Sangria",
+    "Scotch",  "Zinfandel", "Prosecco",   "Cognac",   "Kahlua",   "Vermouth",
+    "Fernet",  "Sake",      "Jager",
+};
+
+const std::array<const char*, 42> alcy_firsts = {
+    "Martina", "Al",       "Chardonnay", "Brandy", "Rose",    "Jim",
+    "Ron",     "Ben",      "Shira",      "Marge",  "Cosmo",   "Lagertha",
+    "April",   "Randy",    "Belinda",    "Julie",  "Renee",   "Sam",
+    "Ryan",    "Fez",      "Zachary",    "Tina",   "Scott",   "Sandy",
+    "Jager",   "Rosemary", "Tito",       "Jim",    "Sheila",  "Vito",
+    "Daniel",  "Morgan",   "Johnnie",    "Walker", "Jose",    "Stella",
+    "Samuel",  "Adam",     "Mike",       "Miller", "Jameson", "Hendrick",
+
+};
+
 const std::array<const char*, 19> first_names = {
     "Buck", "Bark",  "Cleef", "Tuck", "Jack", "Sal",  "Clove",
     "Beef", "Bart",  "Ted",   "Ned",  "Kyle", "Myle", "Mark",
@@ -14,9 +35,9 @@ const std::array<const char*, 8> last_names = {"Buckle", "Chuckle", "Stedman",
                                                "Amore",  "Fishman"};
 
 static std::string get_random_name() {
-    int first = randIn(0, (int)first_names.size() - 1);
-    int last = randIn(0, (int)last_names.size() - 1);
-    return fmt::format("{} {}", first_names[first], last_names[last]);
+    int first = randIn(0, (int) alcy_firsts.size() - 1);
+    int last = randIn(0, (int) alcohol_names.size() - 1);
+    return fmt::format("{} {}", alcy_firsts[first], alcohol_names[last]);
 }
 
 // TODO full names (special customers/doctors?)
