0ab1a0efecf7f28f3f7017e61093c93abebe7838
Gabe Ochoa <gabeochoa@meta.com>
2023-10-19 21:20:36 -0400

    design - make trash default and mop not

--- src/map_generation.h
+++ src/map_generation.h
@@ -250,7 +250,7 @@ struct helper {
         validate_exist(EntityType::FastForward);
         validate_exist(EntityType::Cupboard);
         validate_exist(EntityType::SodaMachine);
-        validate_exist(EntityType::MopHolder);
+        validate_exist(EntityType::Trash);
 
         // ensure customers can make it to the register
         {
