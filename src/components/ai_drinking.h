26e3000b511a5f1f18066e74f18cf797327fa1f4
Gabe Ochoa <gabeochoa@meta.com>
2024-04-13 15:22:01 -0400

    design - customer finds a place within 10 blocks to drink

--- src/components/ai_drinking.h
+++ src/components/ai_drinking.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../engine/log.h"
+#include "../entity_query.h"
 #include "ai_component.h"
 #include "base_component.h"
 
@@ -8,13 +9,34 @@ struct AIDrinking : public AIComponent {
     struct AIDrinkingTarget : AITarget {
         explicit AIDrinkingTarget(const ResetFn& resetFn) : AITarget(resetFn) {}
 
-        virtual OptEntity find_target(const Entity&) override {
+        virtual OptEntity find_target(const Entity& customer) override {
             // TODO :MAKE_DURING_FIND: need a way to clean this up on unset
             auto& entity = EntityHelper::createEntity();
+
+            vec2 position = vec2{0, 0};
+
+            int attempts_rem = 100;
+            while (attempts_rem > 0) {
+                attempts_rem--;
+
+                vec2 new_pos = vec2{
+                    randfIn(-10, 10),
+                    randfIn(-10, 10),
+                };
+
+                bool has_values = EntityQuery()
+                                      .whereHasComponent<IsSolid>()
+                                      .whereSnappedPositionMatches(new_pos)
+                                      .has_values();
+
+                if (!has_values) {
+                    position = customer.get<Transform>().as2() + new_pos;
+                    break;
+                }
+            }
+
             // TODO in debug mode add renderer for ai target location?
-            convert_to_type(EntityType::AITargetLocation, entity,
-                            // TODO choose a better place
-                            vec2{0, 0});
+            convert_to_type(EntityType::AITargetLocation, entity, position);
             return entity;
         }
     } target;
