e605a63dbf59ba6d99a714743099b9eb374d8778
Gabe Ochoa <gabeochoa@meta.com>
2024-04-25 21:01:00 -0400

    be - migrate to using renderinfo class instead of having 10 params kinda

--- src/engine/ui/ui_internal.h
+++ src/engine/ui/ui_internal.h
@@ -84,52 +84,75 @@ inline void draw_text(const TranslatableString& content, Rectangle parent,
                       active_theme().from_usage(color_usage));
 }
 
-inline void draw_rect_color(Rectangle rect, int z_index, Color c,
-                            // TODO replace with RenderFlags
-                            bool rounded) {
+struct RectRenderInfo {
+    struct RoundedInfo {
+        bool rounded = false;
+        float roundness = 0.25f;
+        int segments = 4;
+    } roundedInfo;
+
+    struct OutlineInfo {
+        float thickness = 5.f;
+        bool outlineOnly = false;
+    } outlineInfo;
+
+    // non defaulted params
+
+    Rectangle rect;
+    int z_index;
+    Color color;
+};
+
+inline void draw_any_rect(const RectRenderInfo& info) {
     callback_registry.register_call(
         context,
         [=]() {
-            if (rounded) {
-                DrawRectangleRounded(rect, 0.25f, 4, c);
+            if (info.roundedInfo.rounded) {
+                if (info.outlineInfo.outlineOnly) {
+                    DrawRectangleRoundedLines(
+                        info.rect, info.roundedInfo.roundness,
+                        info.roundedInfo.segments, info.outlineInfo.thickness,
+                        info.color);
+                } else {
+                    DrawRectangleRounded(info.rect, info.roundedInfo.roundness,
+                                         info.roundedInfo.segments, info.color);
+                }
             } else {
-                DrawRectangleRec(rect, c);
+                if (info.outlineInfo.outlineOnly) {
+                    DrawRectangleLinesEx(info.rect, info.outlineInfo.thickness,
+                                         info.color);
+                } else {
+                    DrawRectangleRec(info.rect, info.color);
+                }
             }
         },
-        z_index);
+        info.z_index);
 }
 
-// TODO eventually combine with draw_rect and just add flags
-inline void draw_rect_outline_color(Rectangle rect, int z_index, Color c,
-                                    // TODO replace with RenderFlags
-                                    bool rounded) {
-    callback_registry.register_call(
-        context,
-        [=]() {
-            // TODO does this need to depend on resolution?
-            float thickness = 5.f;
-            if (rounded) {
-                DrawRectangleRoundedLines(rect, 0.25f, 4, thickness, c);
-            } else {
-                DrawRectangleLinesEx(rect, thickness, c);
-            }
-        },
-        z_index);
+inline void draw_rect_color(Rectangle rect, int z_index, Color color,
+                            bool rounded = false, bool outlineOnly = false) {
+    draw_any_rect(RectRenderInfo{
+        .rect = rect,
+        .z_index = z_index,
+        .color = color,
+
+        //
+        .roundedInfo =
+            RectRenderInfo::RoundedInfo{
+                .rounded = rounded,
+            },
+        .outlineInfo =
+            RectRenderInfo::OutlineInfo{
+                .outlineOnly = outlineOnly,
+            },
+    });
 }
 
 inline void draw_rect(Rectangle rect, int z_index,
                       ui::theme::Usage color_usage = ui::theme::Usage::Primary,
-                      bool rounded = false) {
+                      bool rounded = false, bool outlineOnly = false) {
     draw_rect_color(rect, z_index, active_theme().from_usage(color_usage),
-                    rounded);
-}
-
-inline void draw_rect_outline(
-    Rectangle rect, int z_index,
-    ui::theme::Usage color_usage = ui::theme::Usage::Primary,
-    bool rounded = false) {
-    draw_rect_outline_color(rect, z_index,
-                            active_theme().from_usage(color_usage), rounded);
+                    rounded, outlineOnly);
 }
 
 inline void draw_image(vec2 pos, raylib::Texture texture, float scale,
@@ -148,8 +171,8 @@ inline void draw_focus_ring(const Widget& widget, bool rounded = false) {
     Rectangle rect = widget.get_rect();
     float pixels = WIN_HF() * 0.003f;
     rect = rect::expand(rect, {pixels, pixels, pixels, pixels});
-    internal::draw_rect_outline(rect, widget.z_index, ui::theme::Usage::Accent,
-                                rounded);
+    internal::draw_rect(rect, widget.z_index, ui::theme::Usage::Accent, rounded,
+                        true /* outline only */);
 }
 
 }  // namespace internal
