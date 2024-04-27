70c43159c9b1708f924cb43e16c39ecc433af34e
Gabe Ochoa <gabeochoa@meta.com>
2024-04-26 22:39:41 -0400

    idea - add idea about upgrade

--- src/dataclass/upgrade_class.h
+++ src/dataclass/upgrade_class.h
@@ -23,6 +23,8 @@ enum struct UpgradeClass {
     DownTheHatch,
     Jukebox,
     CantEvenTell,
+    // TODO OneForYou - more tips but every customer also gives you a drink
+    // which makes it ahrder to walk around or somethign
 };
 
 using UpgradeClassBitSet = std::bitset<magic_enum::enum_count<UpgradeClass>()>;
