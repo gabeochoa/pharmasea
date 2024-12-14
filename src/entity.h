57f8a9deb2a84fc4598ed6b8080bfc4ac829fc75
Gabe Ochoa <gabeochoa@meta.com>
2024-12-13 20:54:21 -0500

    use afterhours opt entity

--- src/entity.h
+++ src/entity.h
@@ -3,6 +3,8 @@
 
 #include "afterhours/ah.h"
 using afterhours::Entity;
+using afterhours::OptEntity;
+using afterhours::RefEntity;
 
 #include "bitsery/ext/std_bitset.h"
 #include "bitsery/ext/std_smart_ptr.h"
@@ -18,40 +20,6 @@ using afterhours::Entity;
 struct DebugOptions {
     EntityType type = EntityType::Unknown;
     bool enableCharacterModel = true;
-};
-
-using RefEntity = std::reference_wrapper<Entity>;
-using OptEntityType = std::optional<std::reference_wrapper<Entity>>;
-
-struct OptEntity {
-    OptEntityType data;
-
-    OptEntity() {}
-    OptEntity(OptEntityType opt_e) : data(opt_e) {}
-    OptEntity(RefEntity _e) : data(_e) {}
-    OptEntity(Entity& _e) : data(_e) {}
-
-    bool has_value() const { return data.has_value(); }
-    bool valid() const { return has_value(); }
-
-    Entity* value() { return &(data.value().get()); }
-    Entity* operator*() { return value(); }
-    Entity* operator->() { return value(); }
-
-    const Entity* value() const { return &(data.value().get()); }
-    const Entity* operator*() const { return value(); }
-    // TODO look into switching this to using functional monadic stuff
-    // i think optional.transform can take and handle calling functions on an
-    // optional without having to expose the underlying value or existance of
-    // value
-    const Entity* operator->() const { return value(); }
-
-    Entity& asE() { return data.value(); }
-    const Entity& asE() const { return data.value(); }
-
-    operator RefEntity() { return data.value(); }
-    operator RefEntity() const { return data.value(); }
-    operator bool() const { return valid(); }
 };
 
 using Item = Entity;
