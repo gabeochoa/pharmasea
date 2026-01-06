1d2e2ee8d96aea690c781687fde9fcf23a72521e
Gabe Ochoa <gabeochoa@meta.com>
2026-01-06 15:36:10 -0600

    refactor RunTimerSystem

--- src/system/afterhours_gamelike_systems.cpp
+++ src/system/afterhours_gamelike_systems.cpp
@@ -58,47 +58,47 @@ struct RunTimerSystem : public afterhours::System<HasDayNightTimer> {
         return GameState::get().is_game_like();
     }
 
+    void execute_close_bar(Entity& entity, HasDayNightTimer& ht) {
+            ht.close_bar();
+            if (entity.is_missing<IsBank>())
+                log_error("system_manager::run_timer missing IsBank");
+            // Upgrading this log to an error because it should never happen
+            // and if it never happens we can add the system filtering 
+
+            // If we have more days until rent is due, we can skip the rent payment
+            if (ht.days_until() > 0) {
+                return;
+            }
+
+            IsBank& isbank = entity.get<IsBank>();
+            if (isbank.balance() < ht.rent_due()) {
+                log_error("you ran out of money, sorry");
+            }
+
+            isbank.withdraw(ht.rent_due());
+            ht.reset_rent_days();
+            // TODO update rent due amount
+            ht.update_amount_due(ht.rent_due() + 25);
+
+            // TODO add a way to pay ahead of time ?? design
+    }
+
+    void execute_open_bar(Entity& entity, HasDayNightTimer& ht) {
+        if (entity.is_missing<IsProgressionManager>())
+            log_error("system_manager::run_timer missing IsProgressionManager");
+        progression::collect_progression_options(entity, dt);
+        // TODO theoretically we shouldnt start until after you choose upgrades
+        // but we are gonna change how this works later anyway i think
+        ht.open_bar();
+    }
+
     virtual void for_each_with(Entity& entity, HasDayNightTimer& ht,
                                float dt) override {
         ht.pass_time(dt);
         if (!ht.is_round_over()) return;
-
-        if (ht.is_bar_open()) {
-            ht.close_bar();
-
-            // TODO we assume that this entity is the same one which should be
-            // the case if so then we should be using the System filtering to
-            // just grab that for now lets just leave this and we can later
-            // bring it in
-            if (entity.is_missing<IsBank>())
-                log_warn("system_manager::run_timer missing IsBank");
-
-            if (ht.days_until() <= 0) {
-                IsBank& isbank = entity.get<IsBank>();
-                if (isbank.balance() < ht.rent_due()) {
-                    log_error("you ran out of money, sorry");
-                }
-                isbank.withdraw(ht.rent_due());
-                ht.reset_rent_days();
-                // TODO update rent due amount
-                ht.update_amount_due(ht.rent_due() + 25);
-
-                // TODO add a way to pay ahead of time ?? design
-            }
-
-            return;
-        }
-
-        // TODO we assume that this entity is the same one which should be the
-        // case if so then we should be using the System filtering to just grab
-        // that for now lets just leave this and we can later bring it in
-        if (entity.is_missing<IsProgressionManager>())
-            log_warn("system_manager::run_timer missing IsProgressionManager");
-        progression::collect_progression_options(entity, dt);
-
-        // TODO theoretically we shouldnt start until after you choose upgrades
-        // but we are gonna change how this works later anyway i think
-        ht.open_bar();
+        ht.is_bar_open() 
+        ?  execute_close_bar(entity, ht)
+        : execute_open_bar(entity, ht);
     }
 };
 
